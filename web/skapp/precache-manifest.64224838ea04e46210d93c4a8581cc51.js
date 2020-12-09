20e328e3adf3c9cd6846d8b73b12aafebb308273
skynethub.io <skynethub.io@gmail.com>
2020-12-08 20:00:44 -0500

    SkySpace Static Website

---
+++ web/skapp/precache-manifest.64224838ea04e46210d93c4a8581cc51.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a500984d7dc1df4ba14469f6e8a5ccce",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e535a485ee464803bbfe",
+    "url": "./static/css/2.bdb5f066.chunk.css"
+  },
+  {
+    "revision": "7ae03e020e1081311d64",
+    "url": "./static/css/main.a99bbec1.chunk.css"
+  },
+  {
+    "revision": "e535a485ee464803bbfe",
+    "url": "./static/js/2.2d8a7759.chunk.js"
+  },
+  {
+    "revision": "f5b07d2c9969c8bffe1aa873f7021282",
+    "url": "./static/js/2.2d8a7759.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ae03e020e1081311d64",
+    "url": "./static/js/main.4b70fbfe.chunk.js"
+  },
+  {
+    "revision": "f26ced862bd0323f3ed1",
+    "url": "./static/js/runtime-main.42b2d879.js"
+  },
+  {
+    "revision": "7dd07c88461579d46c7af891d4c92a1e",
+    "url": "./static/media/Sia.7dd07c88.svg"
+  },
+  {
+    "revision": "531bd02888f509c0b14c490bdaae2f2e",
+    "url": "./static/media/SkySpaces_g.531bd028.png"
+  },
+  {
+    "revision": "1f88e98a80a5eb3d425fa004e413b05e",
+    "url": "./static/media/SkySpaces_logo.1f88e98a.png"
+  },
+  {
+    "revision": "ffb0dfa9e685c20ebfe25b4677cec695",
+    "url": "./static/media/SkySpaces_logo_transparent_small.ffb0dfa9.png"
+  },
+  {
+    "revision": "a0153b0e786d26224fa27191b2baa39d",
+    "url": "./static/media/icon.a0153b0e.jpeg"
+  },
+  {
+    "revision": "ffb0dfa9e685c20ebfe25b4677cec695",
+    "url": "./static/media/smLogo.ffb0dfa9.png"
+  }
+]);
